e95e3a9e0fa2f332ce7cfc846ab8ef1030201604
Zhou <zhouqiang@wandoujia.com>
2017-10-26 20:22:28 +0800

    add depth

---
+++ depth/depth.py
@@ -0,0 +1,39 @@
+import copy
+from models.order import OrderItem
+
+class Depth(object):
+
+    @staticmethod
+    def parse_depth(asks, bids):
+        asks = sorted(asks, key= lambda ask: ask.price)
+        bids = sorted(bids, key= lambda bid: bid.price, reverse=True)
+        bid_bucket = None
+        ask_bucket = None
+        total = {
+            "depth":0,
+            "ask_eth":0,
+            "bid_eth":0
+        }
+        depth = []
+        while True:
+            try:
+                if not ask_bucket or ask_bucket.amount == 0:
+                    ask_bucket = asks[0]
+                    asks = asks[1:]
+                if not bid_bucket or bid_bucket.amount == 0:
+                    bid_bucket = bids[0]
+                    bids = bids[1:]
+            except:
+                return depth
+            if ask_bucket.price >= bid_bucket.price:
+                return depth
+            decrese_amount = min(bid_bucket.amount, ask_bucket.amount)
+            bid_bucket.amount -= decrese_amount
+            ask_bucket.amount -= decrese_amount
+            total["depth"] += decrese_amount
+            total["ask_eth"] += decrese_amount*ask_bucket.price
+            total["bid_eth"] += decrese_amount*bid_bucket.price
+            total["profit"] = "{0:.2f}%".format((total["bid_eth"] - total["ask_eth"])/total["bid_eth"]*100)
+            t = copy.copy(total)
+            depth.append(t)
+token_depth = Depth()
