0efef356435ce3d05b7c2f1d59668cbb59d0565e
Zhou <zhouqiang@wandoujia.com>
2017-10-26 21:11:15 +0800

    format output

--- tools/scan.py
+++ tools/scan.py
@@ -12,7 +12,7 @@ def get_profit_tokens_and_report():
             t.get_token_order_info(["liqui","hitbtc"])
             item = t.summary()
             print item
-            if item["profit"] > 1:
+            if item["profit"] > 2:
                 report_with_depth(item)
         except Exception as e:
             print str(e)
