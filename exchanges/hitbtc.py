f392901aa70a1fcd4b0202e36057fcae4a7d4461
Zhou <zhouqiang@wandoujia.com>
2017-10-28 10:52:09 +0800

    fix some bugs

--- exchanges/hitbtc.py
+++ exchanges/hitbtc.py
@@ -43,16 +43,21 @@ class Hitbtc(Exchange):
 
     def order(self,symbol, side, price,quantity):
         """/api/1/trading/new_order"""
-        key = HITBTC["key"]
-        secret = HITBTC["secret"]
-        nonce = str(int(time.mktime(datetime.datetime.now().timetuple()) * 1000 + datetime.datetime.now().microsecond / 1000))
-        clientOrderId = "".join(random.choice(string.digits + string.ascii_lowercase) for _ in range(30))
-        path = "/api/1/trading/new_order?apikey=" + key + "&nonce=" + nonce
-        newOrder = "clientOrderId=" + clientOrderId + "&symbol={symbol}&side={side}&price={price}&quantity={quantity}&type=limit".format(symbol=symbol,side=side,price=price,quantity=quantity)
-        signature = hmac.new(secret, path + newOrder, hashlib.sha512).hexdigest()
-        result = self.post("http://api.hitbtc.com" + path, headers={"Api-Signature": signature}, data=newOrder)
+        # key = HITBTC["key"]
+        # secret = HITBTC["secret"]
+        # nonce = str(int(time.mktime(datetime.datetime.now().timetuple()) * 1000 + datetime.datetime.now().microsecond / 1000))
+        # clientOrderId = "".join(random.choice(string.digits + string.ascii_lowercase) for _ in range(30))
+        # path = "/api/1/trading/new_order?apikey=" + key + "&nonce=" + nonce
+        # newOrder = "clientOrderId=" + clientOrderId + "&symbol={symbol}&side={side}&price={price}&quantity={quantity}&type=limit".format(symbol=symbol,side=side,price=price,quantity=quantity)
+        # signature = hmac.new(secret, path + newOrder, hashlib.sha512).hexdigest()
+        # result = self.post("http://api.hitbtc.com" + path, headers={"Api-Signature": signature}, data=newOrder)
         # print result.body['ExecutionReport']
-        print result.body
+        import requests
+
+        orderData = {'symbol':'snceth', 'side': 'buy', 'quantity': '0.005', 'price': '0.00012' }
+        r = requests.post('https://api.hitbtc.com/api/2/order', data = orderData, auth=(key, secret))        
+        print(r.json())
+        # print result.body
 
 if __name__ == "__main__":
     h = Hitbtc()
