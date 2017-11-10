e4802ed5cb278d998696846708ea6476cf271464
Zhou <zhouqiang@wandoujia.com>
2017-11-10 20:30:53 +0800

    add bithum

---
+++ exchanges/bithumb.py
@@ -0,0 +1,33 @@
+import json
+import models
+import hmac
+import hashlib
+import time
+# import requests
+from exchanges.common import Exchange
+from settings.account import BINANCE
+try:
+    from urllib.parse import urlencode
+except ImportError:
+    from urllib import urlencode
+
+
+class Bithumb(Exchange):
+    def __init__(self):
+        self._prefix = "https://api.bithumb.com"
+
+    def get_symbols(self):
+        url = self._prefix  + "/public/ticker/ALL"
+        symbols = [ item for item in json.loads(self._fetch(url))["data"] ]
+        print symbols
+        #raw_items = filter(lambda x:x.endswith("ETH"), symbols)
+        #return [ item[:-3].lower() for item in raw_items ]
+
+
+if __name__ == "__main__":
+    b = Binance()
+    # b.withdraw("enj","0x7f59fbfe6C2cBA95173d69B4B0B00E09c76501FC",1000)
+    b.order("enj","buy","0.00005","1000")
+    #print b.get_symbols()
+    # print b.get_token_orders("eos")
+    #print b.get_all_price()
