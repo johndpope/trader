fa547adc17b40b0ea59b600812c95c15631fafd6
Zhou Qiang <zhouqiang.cl@gmail.com>
2017-11-10 19:54:56 +0800

    new token

---
+++ exchangetools/new_token.py
@@ -0,0 +1,16 @@
+import time
+from account.scanner import account_scanner
+from exchangetools.report import report_token
+
+def main():
+    while True:
+        new_tokens = account_scanner.scan_all_exchanges()
+        for exchange, tokens in new_tokens.items():
+            if tokens:
+                report_token(exchange, tokens)
+        time.sleep(1)
+        # for token_pair in token_pairs:
+        #     balance._balance_between_account(token_pair["from"], token_pair["to"], token_pair["amount"])
+
+if __name__ == "__main__":
+    main()
