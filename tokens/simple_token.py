a519b8f755aec5995efaed16cd3a71bd5ee4875e
Zhou <zhouqiang@wandoujia.com>
2017-11-11 22:03:34 +0800

    add token

--- tokens/simple_token.py
+++ tokens/simple_token.py
@@ -1,4 +1,7 @@
 # -*- coding=utf-8 -*-
+import tornado.ioloop
+import tornado.web
+from datetime import date
 from libs.db import aquire_cursor
 
 class Tokens(object):
@@ -19,6 +22,27 @@ class Tokens(object):
             d["create_time"] = str(item["create_time"])
             ret.append(d)
         return ret
+
+token_model = Tokens()
+class TokenTimeLineHandler(tornado.web.RequestHandler):
+    def get(self):
+        token = self.get_argument("token","knc")
+        ret = token_model.get_token_timeline(token)
+        # today = date.today()
+        self.finish(ret)
+
+def make_app():
+    return tornado.web.Application([
+        (r"/api/v1/token/timeline", TokenTimeLineHandler),
+    ],
+    #debug = True,
+    )
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
+
 if __name__ == "__main__":
     t = Tokens()
     print t.get_token_timeline("rdn")
