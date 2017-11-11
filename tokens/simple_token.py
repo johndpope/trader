3baf103907f57db0d03600c817dcef4feb15a8bb
Zhou <zhouqiang@wandoujia.com>
2017-11-11 21:51:29 +0800

    add timeline

--- tokens/simple_token.py
+++ tokens/simple_token.py
@@ -2,10 +2,6 @@
 from libs.db import aquire_cursor
 
 class Tokens(object):
-    """docstring for Tokens"""
-    def __init__(self, arg):
-        super(Tokens, self).__init__()
-        self.arg = arg
 
     def get_token_timeline(self, token):
         with aquire_cursor() as cursor:
